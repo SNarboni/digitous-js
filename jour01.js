ea8b8a0af579a4a28ad0fbe66743dc8a67fda09b
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-07 16:56:44 +0100

    exo bonus fini

--- jour01.js
+++ jour01.js
@@ -2,19 +2,19 @@
 console.log("Hello World !");
 
 // 02 - String
-var test = "My name is xxx"
+var test = "My name is xxx";
 console.log(test);
 
 // 03 - Concatenation
-var name = "Seb"
+var name = "Seb";
 console.log(`Nice to meet you ${name} !`);
 
 // 04 - String Length
 var testLength = "I'm very long !".length;
-console.log(testLength)
+console.log(testLength);
 
 // 05 - Replace
-var food = "croissant is meh"
+var food = "croissant is meh";
 console.log(food.replace("meh", "so good"));
 
 // 06 - Up and Down (J'ai pas trop compris l'énoncé)
@@ -27,11 +27,14 @@ console.log(basic.toLowerCase());
 
 // 07 - Split
 var word = "banana";
-var letters = word.split("")
-console.log(letters)
+var letters = word.split("");
+console.log(letters);
 
 // 08 - Template
-var age = "21"
+var age = "21";
 var template = `I'm ${age} years old`;
-console.log(template)
+console.log(template);
+
 // Bonus
+var test = 'Bonjour';
+console.log(test.replace(/o/gi, 'a'));
