ed9e4b46b18e9359b034b0fde8226832c0babe80
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-07 14:32:38 +0100

    exo 1,2,3,4,5,6, fini

---
+++ jour01.js
@@ -0,0 +1,30 @@
+// 01 - Hello World
+console.log("Hello World !");
+
+// 02 - String
+var test = "My name is xxx"
+console.log(test);
+
+// 03 - Concatenation
+var name = "Seb"
+console.log(`Nice to meet you ${name} !`);
+
+// 04 - String Length
+var testLength = "I'm very long !".length;
+console.log(testLength)
+
+// 05 - Replace
+var food = "croissant is meh"
+console.log(food.replace("meh", "so good"));
+
+// 06 - Up and Down (J'ai pas trop compris l'énoncé)
+var basic = "This is Cool"
+// var basicUp = "THIS IS COOL"
+// var basicDown = "this is cool"
+console.log(basic);
+console.log(basic.toLocaleUpperCase());
+console.log(basic.toLowerCase());
+
+// 07 - Split
+// 08 - Template
+// Bonus
