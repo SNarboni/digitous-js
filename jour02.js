5e0c758331bbf853cdf173989ad7c6d243a6bd12
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-08 14:10:17 +0100

    exo 1 fini

---
+++ jour02.js
@@ -0,0 +1,17 @@
+// 01 - Number
+var integer = 102;
+var float = 13.9;
+var result = integer + float;
+console.log(`Le résultat est = ${result}`);
+
+// 02 - Convert
+// 03 - Round
+// 04 - Arithmétique
+// 05 - Comparaison
+// 06 - Condition
+// 07 - Condition II
+// 08 - Condition III
+// Bonus I
+// Bonus II
+// Bonus III
+// Bonus IV
