45ba02d895374b75bdbbee35fb1938b624499e36
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-08 14:26:56 +0100

    exo 3 fini

--- jour02.js
+++ jour02.js
@@ -5,18 +5,39 @@ var result = integer + float;
 console.log(`Le résultat est = ${result}`);
 
 // 02 - Convert
-var basic = 34
-var stringified = basic.toString()
-console.log(stringified)
-console.log(stringified.length)
+var basic = 34;
+var stringified = basic.toString();
+console.log(stringified);
+console.log(stringified.length);
 
 // 03 - Round
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
+
 // 04 - Arithmétique
+
+
 // 05 - Comparaison
+
+
 // 06 - Condition
+
+
 // 07 - Condition II
+
+
 // 08 - Condition III
+
+
 // Bonus I
+
+
 // Bonus II
+
+
 // Bonus III
+
+
 // Bonus IV
+
