5b05cdc2f20fd9d95001aabc0044a2ad6fba0aaf
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-09 16:19:52 +0100

    exo bonus I fini

--- jour03.js
+++ jour03.js
@@ -1,14 +1,20 @@
 // 01 - Array
+console.log("--------01--------");
+
 var fruits = ["mango", "lemon", "blueberry"];
 console.log(fruits);
 console.table(fruits);
 
 // 02 - Access
+console.log("--------02--------");
+
 var ingredients = ["eggs", "milk", "butter"];
 console.log(ingredients[1]);
 console.log(ingredients.indexOf("butter"));
 
 // 03 - Add and Remove
+console.log("--------03--------");
+
 var objects = ["pen", "book", "lamp"];
 
 objects.unshift("chair");
@@ -24,11 +30,15 @@ objects.shift("chair");
 console.log('supp de "chair": ' + objects);
 
 // 04 - Order
+console.log("--------04--------");
+
 var numbers = [4, 10, 8, 12, 6];
 console.log("A l'envers: " + numbers.reverse());
 console.log("Croissant: " + numbers.sort((a, b) => a - b));
 
 // 05 - Boucle
+console.log("--------05--------");
+
 var limit = 10;
 for (var total = 0; total !== limit; total++) {
     console.log("inside: " + total)
@@ -36,25 +46,45 @@ for (var total = 0; total !== limit; total++) {
 console.log("outside: " + total)
 
 // 06 - Reverse
+console.log("--------06--------");
+
 function retourne(seb) {
     var retournemot = "";
     for (var i = seb.length - 1; i >= 0; i--) {
-        retournemot = retournemot + seb[i];
+        retournemot += seb[i];
     }
     return retournemot;
-
 }
 console.log(retourne("Hello konexio !"));
 
 
 
 // Bonus
+console.log("--------B1--------");
 
+for (var nbr = 1; nbr <= 100; nbr++) {
+    var affiche = "";
+    if (nbr % 3 === 0 && nbr % 5 === 0) {
+        affiche = 'fizzbuzz';
+    } else if (nbr % 3 === 0) {
+        affiche = 'fizz';
+    } else if (nbr % 5 === 0) {
+        affiche = 'buzz';
+    } else if (nbr % 7 === 0) {
+        affiche = 'coucou';
+    }
+    console.log(nbr + " : " + affiche);
+}
 
 // Bonus II
+console.log("--------B2--------");
+
 
 
 // Bonus III
+console.log("--------B3--------");
+
 
 
 // Bonus IV
+console.log("--------B4--------");
