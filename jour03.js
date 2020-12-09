a2962b88754305476eba7eb111116ce79fdc15e8
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-09 14:44:54 +0100

    exo 5 fini

--- jour03.js
+++ jour03.js
@@ -9,7 +9,7 @@ console.log(ingredients[1]);
 console.log(ingredients.indexOf("butter"));
 
 // 03 - Add and Remove
-var objects = ["pen", "book", "lamp"]
+var objects = ["pen", "book", "lamp"];
 
 objects.unshift("chair");
 console.log('Ajout de "chair": ' + objects);
@@ -24,12 +24,16 @@ objects.shift("chair");
 console.log('supp de "chair": ' + objects);
 
 // 04 - Order
-var numbers = [4, 10, 8, 12, 6]
-console.log("A l'envers: " + numbers.reverse())
+var numbers = [4, 10, 8, 12, 6];
+console.log("A l'envers: " + numbers.reverse());
 console.log("Croissant: " + numbers.sort((a, b) => a - b));
 
 // 05 - Boucle
-
+var limit = 10;
+for (var total = 0; total !== limit; total++) {
+    console.log("inside: " + total)
+}
+console.log("outside: " + total)
 
 // 06 - Reverse
 
