246b2f45eb48aaf526424529c3c57af1e1e4fdff
SNarboni <sebastien.narboni@hotmail.fr>
2020-12-14 13:56:42 +0100

    exo 3 fini

--- jour04.js
+++ jour04.js
@@ -33,11 +33,25 @@ console.log("-------------------")
 // 03 - Even
 console.log("----EXO 03----")
 
+function checkIfEven(num) {
+    if (num % 2 === 0) {
+        console.log("even")
+    } else {
+        console.log("odd")
+    }
+}
+checkIfEven(2);
+checkIfEven(3);
+checkIfEven(4);
+checkIfEven(5);
 
 console.log("-------------------")
 // 04 - Compare
 console.log("----EXO 04----")
 
+function compare(num1, num2) {
+
+}
 
 console.log("-------------------")
 // 05 - Add Up
